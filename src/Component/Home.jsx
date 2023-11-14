ae131b24aefb42103b389213c56cd35e085d92fc
mdshahjhan <mdshahjha478877@gmail.com>
2023-11-15 02:34:45 +0600

    shahjhan

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -1,41 +1,46 @@
 
-import { Typewriter,Cursor } from 'react-simple-typewriter'
+import { Typewriter, Cursor } from 'react-simple-typewriter'
 import img1 from '../assets/my-bg2 (2).png'
-import { FaSquareGithub,  } from "react-icons/fa6";
-import { Link } from 'react-router-dom';
+import { FaSquareGithub, } from "react-icons/fa6";
+import '../Component/Home.css'
+import { useState } from 'react';
 const Home = () => {
-    const text =   Typewriter({
-        words:['Web Developer','Mern Stack Developer','React Developer'],
-        loop:{}
-       
+    const [isHovered, setIsHovered] = useState(false);
+    const text = Typewriter({
+        words: ['Web Developer', 'Mern Stack Developer', 'React Developer'],
+        loop: {}
+
     })
     return (
         <div>
             <section className="dark:bg-gray-800 dark:text-gray-100">
                 <div className="container flex flex-col justify-center p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between">
-                 <div className='pt-20 ml-10'>
-                    <h1 className='text-5xl font-semibold' style={{margin:'50px'}}>
-                       <p className='text-primary text-2xl'>Hi !</p>
-                       
-                       <p>I’M SHAHJHAN MIAH</p>
-                        I am    
+                    <div className='pt-20 ml-10'>
+                        <h1 className='text-5xl font-semibold' style={{ margin: '50px' }}>
+                            <p className='text-primary text-2xl'>Hi !</p>
 
-                        <span style={{fontWeight:'bold',color:'orange'}}>
-                            {text}
+                            <p>I’M SHAHJHAN MIAH</p>
+                            I am
 
-                            <span style={{color:'red'}}>
-                            <Cursor cursorStyle='>'></Cursor>
-                        </span>
-                         
-                        </span>
-                        <Link to={'/https://github.com/Shahjhanmiah'}> <FaSquareGithub></FaSquareGithub></Link>
-                       
-                    </h1>
-                 </div>
+                            <span style={{ fontWeight: 'bold', color: 'orange' }}>
+                                {text}
+
+                                <span style={{ color: 'red' }}>
+                                    <Cursor cursorStyle='>'></Cursor>
+                                </span>
+
+                            </span>
+                            <div className= {`icon-container ${isHovered ? 'hovered' : ''}`} onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
+                               <FaSquareGithub></FaSquareGithub>
+                            </div>
+                        </h1>
+                    </div>
                     <div className="flex flex-col justify-center p-6 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left  pt-10">
-                       
-                    <figure><img className='pt10 pr-10 mr-10' src={img1} alt="Shoes" /></figure>
-                    
+
+                        <figure> <div className="image-container" onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
+                            <img src={img1} alt="Image" className={isHovered ? 'hovered' : ''} />
+                        </div></figure>
+
                     </div>
                 </div>
             </section>
